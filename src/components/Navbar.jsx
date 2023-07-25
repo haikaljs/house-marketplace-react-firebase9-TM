b9595a29c18b0c5c2878b5c1ac0ee6e3b4d9dad2
haikaljs <haikaljavascript@gmail.com>
2023-07-25 19:17:43 +0800

    Sign in & sign up forms

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
           </li>
           <li className="navbarListItem" onClick={() => navigate("/offers")}>
             <OfferIcon
-              fill={pathMatchRoute("/offers") ? "#2c2c2c" : "#8f8f8f"}
+              fill={pathMatchRoute("/offers") ? "#2c2c2c" : "8f8f8f"}
               width="36px"
               height="36px"
             />
